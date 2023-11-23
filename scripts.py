f113b61f87b87eadd202b08be74774d329ff55dd
DamirMustafin <mustafindm@oiate.ru>
2023-11-24 00:12:07 +0300

    Рефакторил код. Добавил скрипт с добавлением столбцов close_price и volume_rub. Добавил расчет этих значений. Добавил файл setting.py на будущее. Что бы поправить базу данных, надо просто запустить файл scripts.py либо метод add_columns_to_ticker из него. Обновил таблицу Excel, туда добавилось UGLD.

--- scripts.py
+++ scripts.py
@@ -9,7 +9,6 @@ import psycopg2
 from dotenv import load_dotenv
 from psycopg2 import sql
 from tinkoff.invest import Client, InstrumentStatus
-from tinkoff.invest.services import InstrumentsService, MarketDataService
 
 load_dotenv()
 
@@ -70,26 +69,58 @@ def update_tables():
             print(error)
 
 
+def add_columns_to_ticker():
+    for index, row in excel_data_df[['figi', 'ticker']].iterrows():
+        try:
+            with conn.cursor() as curs:
+                query = sql.SQL(
+                    """ALTER TABLE {ticker} ADD COLUMN IF NOT EXISTS close_price float;
+                        ALTER TABLE {ticker} ADD COLUMN IF NOT EXISTS volume_rub float;""").format(
+                    ticker=sql.Identifier(row['ticker']))
+
+                curs.execute(query)
+                conn.commit()
+        except Exception as error:
+            print(row[0])
+            print(error)
+
+
+add_columns_to_ticker()
+
+
 def make_mapping():
     with conn.cursor() as curs:
         curs.execute(f"""CREATE TABLE IF NOT EXISTS ticker_mapping (
                            ticker varchar(255),
                            figi varchar(255),
                            name varchar(255),
+                           lot integer,
+                           short_enabled_flag boolean,
+                           units integer,
+                           nano integer,
                            PRIMARY KEY (ticker));""")
         conn.commit()
 
-    for index, row in excel_data_df[['figi', 'ticker', 'name']].iterrows():
+    for index, row in excel_data_df[
+        ['figi', 'ticker', 'name', 'lot', 'short_enabled_flag', 'units', 'nano']].iterrows():
         query = sql.SQL(
-            """insert into ticker_mapping values ({ticker}, {figi}, {name}) 
+            """insert into ticker_mapping values ({ticker}, {figi}, {name}, {lot}, {short_enabled_flag}, {units}, {nano}) 
             on conflict (ticker) do update SET 
                 ticker = EXCLUDED.ticker, 
                 figi = EXCLUDED.figi, 
-                name = EXCLUDED.name""").format(
+                name = EXCLUDED.name,
+                lot = EXCLUDED.lot, 
+                short_enabled_flag = EXCLUDED.short_enabled_flag, 
+                units = EXCLUDED.units,
+                nano = EXCLUDED.nano""").format(
             ticker=sql.Literal(row['ticker']),
             figi=sql.Literal(row['figi']),
-            name=sql.Literal(row['name']))
-        print(query.as_string(conn))
+            name=sql.Literal(row['name']),
+            lot=sql.Literal(row['lot']),
+            short_enabled_flag=sql.Literal(row['short_enabled_flag']),
+            units=sql.Literal(row['units']),
+            nano=sql.Literal(row['nano']))
+
         try:
             with conn.cursor() as curs:
                 curs.execute(query)
